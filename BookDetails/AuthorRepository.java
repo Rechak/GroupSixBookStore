5f6ac0f8aad1f90dc60108dcfb9969c4febb970d
Saul Castillo <110940651+SAOOOL@users.noreply.github.com>
2023-04-03 11:17:02 -0400

    Delete BookDetails directory

--- BookDetails/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package group6.bookstore.BookDetails;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-    Book findByFirstName(String firstName);
-}
